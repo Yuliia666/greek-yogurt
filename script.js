08242fbc2db27c523dc917473387f4c92ea83d10
home2ego <juilia.yakina190@gmail.com>
2024-01-02 20:38:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.try').addEventListener('click', function () {
+  let name = prompt('What is your name?');
+  if (name.length) {
+    alert(
+      'Thanks, ' +
+        name +
+        ', we will be in touch. Meanwhile, have a lot of Yogurt'
+    );
+  } else {
+    alert('No name, no Greek Yogurt for you!');
+  }
+});
